6e44382d59578fc180f60276cfa3c8c2f75b7765
William Pannell <lpann5@gmail.com>
2020-04-06 23:44:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,12 +10,20 @@ class Doctor
     @@all
   end
 
+<<<<<<< HEAD
   def new_appointment(patient, date)
+=======
+  def new_appointment(date, patient)
+>>>>>>> daf038c783008b22f9f22625676c3534d0b9241f
     Appointment.new(date, self, patient)
   end
 
   def appointments
+<<<<<<< HEAD
     Appointment.all.select do |appointment|
+=======
+    Appointments.all.select do |appointment|
+>>>>>>> daf038c783008b22f9f22625676c3534d0b9241f
       appointment.doctor==self
     end
   end
